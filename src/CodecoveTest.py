562f5a9d22cf3cbe5f84327000757195159dced0
Tereshchenko Vlad <ddiavall@yandex.ru>
2024-03-02 20:59:09 +0300

    [feat]: added wrong test class for CI/CD configuration

--- src/CodecoveTest.py
+++ src/CodecoveTest.py
@@ -2,3 +2,6 @@ class CodecoveTest:
     @classmethod
     def calc(self):
         return 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+
+    def mul(self):
+        return 2
