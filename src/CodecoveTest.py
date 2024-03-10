97b2bcfee6873c43367523791d5280b325780244
Tereshchenko Vlad <ddiavall@yandex.ru>
2024-03-10 20:58:54 +0300

    [feat]: modified class for pep checker

--- src/CodecoveTest.py
+++ src/CodecoveTest.py
@@ -6,6 +6,6 @@ class CodecoveTest:
     def mul(self):
         return 2
 
-    # long incorrect comment long incorrect comment long incorrect comment long incorrect comment long incorrect comment
+    # long incorrect comment long incorrect comment long incorrect comment long incorrect comment long incorrect comment long incorrect comment
     def div(self):
         return 0
