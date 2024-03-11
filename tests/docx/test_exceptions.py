e764b185cb2f76bc066717986b82df6db7da9120
MrAmfix <nat.rud.ufa@gmail.com>
2024-03-12 00:30:53 +0300

    [fix]: found paths to files with os.path

--- tests/docx/test_exceptions.py
+++ tests/docx/test_exceptions.py
@@ -3,19 +3,22 @@ from src.docx.exceptionsdocx import *
 from src.docx.docxredactor import DOCXRedactor
 from src.docx.enum.color import Color
 from docx.opc.exceptions import PackageNotFoundError
+from os.path import join, dirname, abspath
 
 
 class TestExceptions(unittest.TestCase):
+    root = join(dirname(dirname(abspath(__file__))), 'docx')
+
     def test_not_supported_format(self):
         with self.assertRaises(NotSupportedFormat):
-            DOCXRedactor('../docx/file.txt')
+            DOCXRedactor(join(self.root, 'file.txt'))
 
     def test_supported_format(self):
         with self.assertRaises(PackageNotFoundError):
-            DOCXRedactor('../docx/not_exist_file.docx')
+            DOCXRedactor(join(self.root, 'not_exists_file.docx'))
 
     def test_correct_file(self):
-        self.assertIsInstance(DOCXRedactor('../docx/file.docx'), DOCXRedactor)
+        self.assertIsInstance(DOCXRedactor(join(self.root, 'file.docx')), DOCXRedactor)
 
     def test_impossible_color(self):
         with self.assertRaises(ImpossibleColor):
@@ -25,12 +28,12 @@ class TestExceptions(unittest.TestCase):
         Color(128, 0, 255)
 
     def test_paragraph_not_found(self):
-        doc = DOCXRedactor('../docx/file.docx')
+        doc = DOCXRedactor(join(self.root, 'file.docx'))
         with self.assertRaises(ParagraphNotFound):
             doc.edit_style_by_id('not_exist_paraId', size=14)
 
     def test_exist_paragraph(self):
-        doc = DOCXRedactor('../docx/file.docx')
+        doc = DOCXRedactor(join(self.root, 'file.docx'))
         para_id = doc.all_para_attributes()[0].values()[0]
         doc.edit_style_by_id(para_id, size=15)
         doc.save()
